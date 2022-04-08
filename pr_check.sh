88192176cbdda80184d112eda1bc5ea1622017a3
Jan-Hendrik Boll <jboll@redhat.com>
2022-04-08 14:14:22 +0200

    Update schema toolchain
    Add tools to update the schema.graphql file

--- pr_check.sh
+++ pr_check.sh
@@ -6,3 +6,6 @@ docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 
 # compile sources and run unit tests
 make build
+
+# verify if schema update causes issues
+make update-schema validate-schema
