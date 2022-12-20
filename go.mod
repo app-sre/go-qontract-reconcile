be7ac0f17e37e2e62b10d2dfecc0fb2d29a9f5d5
Jan-Hendrik Boll <jboll@redhat.com>
2022-12-20 12:00:39 +0100

    Add Runtime metrics, similar to qontract-reconcile

--- go.mod
+++ go.mod
@@ -53,7 +53,9 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.13.9 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.17.6 // indirect
 	github.com/aws/smithy-go v1.13.5 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cenkalti/backoff/v3 v3.0.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/cloudflare/circl v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.13.0 // indirect
@@ -83,6 +85,7 @@ require (
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/mitchellh/copystructure v1.0.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/go-testing-interface v1.0.0 // indirect
@@ -92,6 +95,10 @@ require (
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pierrec/lz4 v2.5.2+incompatible // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.14.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
--- pkg/reconcile.go
+++ pkg/reconcile.go
@@ -2,9 +2,12 @@ package pkg
 
 import (
 	"context"
+	"net/http"
 	"os"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/spf13/viper"
 )
 
@@ -77,20 +80,50 @@ type ResourceState struct {
 	Desired interface{}
 }
 
+type integrationRunnerMetrics struct {
+	status prometheus.Gauge
+	time   prometheus.Gauge
+}
+
+func newIntegrationRunnerMetrics(reg prometheus.Registerer, integration string) *integrationRunnerMetrics {
+	labels := prometheus.Labels{"integration": integration}
+
+	m := &integrationRunnerMetrics{
+		status: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name:        "qontract_reconcile_last_run_status",
+			Help:        "Last run status",
+			ConstLabels: labels,
+		}),
+		time: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name:        "qontract_reconcile_last_run_seconds",
+			Help:        "Last run duration in seconds",
+			ConstLabels: labels,
+		}),
+	}
+	reg.MustRegister(m.status)
+	reg.MustRegister(m.time)
+	return m
+}
+
 // IntegrationRunner is an implementation of Runner
 type IntegrationRunner struct {
 	Runnable Integration
 	Name     string
 	config   *runnerConfig
+	metrics  *integrationRunnerMetrics
+	registry *prometheus.Registry
 }
 
 // NewIntegrationRunner creates a IntegrationRunner for a given Integration
 func NewIntegrationRunner(runnable Integration, name string) *IntegrationRunner {
 	c := newRunnerConfig()
+	registry := prometheus.NewRegistry()
 	v := &IntegrationRunner{
 		Runnable: runnable,
 		Name:     name,
 		config:   c,
+		registry: registry,
+		metrics:  newIntegrationRunnerMetrics(registry, name),
 	}
 	return v
 }
@@ -136,14 +169,23 @@ func (i *IntegrationRunner) runIntegration() {
 }
 
 func (i *IntegrationRunner) Run() {
+	go func(i *IntegrationRunner) {
+		http.Handle("/metrics", promhttp.HandlerFor(i.registry, promhttp.HandlerOpts{Registry: i.registry}))
+		Log().Fatal(http.ListenAndServe(":8080", nil))
+	}(i)
+
 	for {
+		start := time.Now()
 		i.runIntegration()
+		end := time.Now()
+		i.metrics.time.Set(end.Sub(start).Seconds())
 		Log().Debugw("Sleeping", "seconds", i.config.SleepDurationSecs)
 		time.Sleep(time.Duration(i.config.SleepDurationSecs) * time.Second)
 	}
 }
 
 func (i *IntegrationRunner) Exiter(exitCode int) {
+	i.metrics.status.Set(float64(exitCode))
 	if i.config.RunOnce {
 		os.Exit(exitCode)
 	} else {
