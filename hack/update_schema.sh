d1650740d2769720f8397dc45353a52b00f628b2
Jan-Hendrik Boll <jboll@redhat.com>
2022-12-22 13:57:00 +0100

    Fix update-schema script, renamed the wrong image

--- hack/update_schema.sh
+++ hack/update_schema.sh
@@ -6,7 +6,7 @@ git_commit=$(git rev-parse HEAD)
 git_commit_timestamp=$(git log -1 --format=%ct ${git_commit})
 output_dir=$(echo $(realpath pwd)/bundle)
 bundle=data.json
-image=quay.io/app-sre/go-qontract-reconcile
+validator_image=quay.io/app-sre/qontract-validator
 qontract_server_image=quay.io/app-sre/qontract-server
 qontract_schemas_image=quay.io/app-sre/qontract-schemas
 
@@ -21,31 +21,31 @@ mkdir -p ${output_dir} ${PWD}/fake_data $PWD/fake_resources
 
 function cleanup {
     rm -rf ${PWD}/fake_data ${PWD}/fake_resources ${PWD}/schemas ${PWD}/graphql-schemas
-    ${container_engine} stop goqr-qr-server
-    ${container_engine} network rm goqr-shared || true
+    ${container_engine} stop go-qontract-reconcile-qr-server
+    ${container_engine} network rm go-qontract-reconcile-shared || true
 }
 trap cleanup EXIT
 
-${container_engine} create --name=goqr-qr-schemas ${qontract_schemas_image}:latest
-${container_engine} cp goqr-qr-schemas:/schemas/. .
-${container_engine} rm goqr-qr-schemas &>/dev/null
+${container_engine} create --name=go-qontract-reconcile-qr-schemas ${qontract_schemas_image}:latest
+${container_engine} cp go-qontract-reconcile-qr-schemas:/schemas/. .
+${container_engine} rm go-qontract-reconcile-qr-schemas &>/dev/null
 
 ${container_engine} run --rm \
     -v ${PWD}/schemas:/schemas:z \
     -v ${PWD}/graphql-schemas:/graphql:z \
     -v ${PWD}/fake_data:/data:z \
     -v ${PWD}/fake_resources:/resources:z \
-    ${image}:latest \
+    ${validator_image}:latest \
     qontract-bundler /schemas /graphql/schema.yml /data /resources ${git_commit} ${git_commit_timestamp} > ${output_dir}/${bundle}
 
 
-[[ $(${container_engine} network ls | grep -w goqr-shared -c) -eq 0  ]] && ${container_engine} network create goqr-shared
+[[ $(${container_engine} network ls | grep -w go-qontract-reconcile-shared -c) -eq 0  ]] && ${container_engine} network create go-qontract-reconcile-shared
 
 ${container_engine} run -it --rm \
     -v ${output_dir}:/bundle:z \
     -p 4000:4000 -d \
-    --name=goqr-qr-server \
-    -e LOAD_METHOD=fs --network=goqr-shared \
+    --name=go-qontract-reconcile-qr-server \
+    -e LOAD_METHOD=fs --network=go-qontract-reconcile-shared \
     -e DATAFILES_FILE=/bundle/${bundle} \
     ${qontract_server_image}:latest
 
@@ -64,6 +64,5 @@ else
     ${container_engine} build --no-cache -t gql:latest -f ./hack/Dockerfile.gql .
 fi
 
-${container_engine} run --rm --network=goqr-shared gql gql-cli \
-    http://goqr-qr-server:4000/graphql --print-schema  > schema.graphql
-
+${container_engine} run --rm --network=go-qontract-reconcile-shared gql gql-cli \
+    http://go-qontract-reconcile-qr-server:4000/graphql --print-schema  > schema.graphql
