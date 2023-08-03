d7f19fd72b4381914e5114e7877a66d06cbabf08
Jan-Hendrik Boll <jboll@redhat.com>
2023-08-03 09:42:12 +0200

    Remove deprecated check

--- pr_check.sh
+++ pr_check.sh
@@ -13,9 +13,3 @@ make build
 
 # verify if schema update causes issues
 make update-schema validate-schema
-
-# We must use the same version as the Terraform provider.
-grep -q 'github.com/ProtonMail/go-crypto v0.0.0-20210428141323-04723f9f07d7' go.mod || {
-    echo "go-crypto package version mismatch!"
-    exit 1
-} >&2
